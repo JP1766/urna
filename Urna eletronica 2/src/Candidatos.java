1ca1d537537216e499d328e3e1b96c75780d0893
JP1766 <33943426+JP1766@users.noreply.github.com>
2017-12-01 20:03:29 -0200

    Add files via upload

---
+++ Urna eletronica 2/src/Candidatos.java
@@ -0,0 +1,11 @@
+
+public class Candidatos {
+
+    private int[][] pontuacao = new int[100][2];
+    private String[] candidato;
+    
+
+    public Candidatos() {
+candidato = new String[]{"oi"};
+    }
+}
